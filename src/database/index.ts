9d28bc7c553502729d549a0ca4da26bc1d1ff9ec
Julian One <julian.roberts.one@gmail.com>
2023-11-03 19:00:10 -0600

    .

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,7 @@
 import { Pool } from 'pg';
 import env from '../env';
+import { UserRecord, isUserRecord } from './types';
+import { User, toUser } from '../authentication/types';
 
 class Database {
   private static instance: Database;
@@ -32,6 +34,20 @@ class Database {
     }
   }
 
+  public async getUserByUsername(username: string): Promise<UserRecord> {
+    const result = await this.query(
+      'SELECT * FROM users WHERE username = $1 LIMIT 1',
+      [username],
+    );
+    if (result.rows.length === 0) {
+      throw new Error('User not found');
+    }
+    if (!isUserRecord(result.rows[0])) {
+      throw new Error('Record is not of the correct type UserRecord')
+    }
+    return result.rows[0];
+  }
+
   public async userExists(username: string, email: string): Promise<boolean> {
     const result = await this.query(
       'SELECT * FROM users WHERE username = $1 OR email = $2 LIMIT 1',
@@ -44,11 +60,18 @@ class Database {
     username: string,
     email: string,
     hashedPassword: string,
-  ): Promise<void> {
-    await this.query(
-      'INSERT INTO users (username, email, password, created_on) VALUES ($1, $2, $3, NOW())',
+  ): Promise<UserRecord> {
+    const result = await this.query(
+      'INSERT INTO users (username, email, password, created_on) VALUES ($1, $2, $3, NOW()) RETURING *',
       [username, email, hashedPassword],
     );
+    if (result.rows.length === 0) {
+      throw new Error('User creation failed');
+    }
+    if (!isUserRecord(result.rows[0])) {
+      throw new Error('Record is not of the correct type UserRecord')
+    }
+    return result.rows[0];
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import env from './env';
 import Database from './database';
 import bcrypt from 'bcrypt';
 import session from 'express-session';
+import { toUser } from './authentication/types';
 
 const app = express();
 
@@ -12,54 +13,86 @@ app.use(
   session({
     secret: env.SECRET,
     resave: false,
-    saveUninitialized: true,
-  }),
+    saveUninitialized: false, // Don't create a session until something is stored
+    cookie: { secure: false }, // Set secure to true if you're using HTTPS
+  })
 );
 
 const database = Database.getInstance();
 
+const auth = (req: Request, res: Response, next: Function) => {
+  if (!req.session.userId) {
+    console.log(`no user id for session: ${req.session}`)
+    return res.status(401).send('You are not authenticated');
+  }
+  next();
+};
+
+
+app.post('/login', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  
+  try {
+    const userRecord = await database.getUserByUsername(username);
+    const user = toUser(userRecord);
+    
+    if (user && await bcrypt.compare(password, user.password)) {
+      req.session.regenerate((err) => {
+        if (err) {
+          return res.status(500).send('Error regenerating session');
+        }
+        req.session.userId = user.id;
+        res.status(200).send('Logged in successfully');
+      });
+    } else {
+      res.status(401).send('Invalid username or password');
+    }
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).send('Error logging in');
+  }
+});
+
 app.post('/register', async (req: Request, res: Response) => {
-  console.log('register session:', req.session);
-
   const { username, email, password } = req.body;
-  // to-do: server side validation
-
+  
   try {
     const userExists = await database.userExists(username, email);
     if (userExists) {
       return res.status(409).send('User already exists.');
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    await database.createUser(username, email, hashedPassword);
+    const dbNewUser = await database.createUser(username, email, hashedPassword);
+    const newUser = toUser(dbNewUser);
 
-    res.status(201).send('User registered successfully.');
+    req.session.regenerate((err) => {
+      if (err) {
+        return res.status(500).send('Error regenerating session');
+      }
+      req.session.userId = newUser.id;
+      res.status(201).send('User registered successfully.');
+    });
   } catch (error) {
     console.error('Error registering new user:', error);
-
     res.status(500).send('Error registering new user.');
   }
 });
 
-app.get('/protected', (req: Request, res: Response) => {
-  console.log('protected session:', req.session);
-
-  if (!req.session.id) {
-    return res.status(401).send('You are not authenticated');
-  }
+app.get('/protected', auth, (req: Request, res: Response) => {
   res.send('Protected content');
 });
 
 app.get('/logout', (req: Request, res: Response) => {
-  console.log('logout session:', req.session);
-
   req.session.destroy((err) => {
     if (err) {
       console.error('Error during session destruction:', err);
       return res.status(500).send('Error logging out');
     }
+    res.clearCookie('connect.sid'); // The name of the cookie to clear
     res.redirect('/');
   });
 });
+
 
 app.get('/register', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'public', 'register.html'));
