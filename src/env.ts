1d26a5d2ec442f82c7d6cf0274896782ea4d6b7c
Julian One <julian.roberts.one@gmail.com>
2023-11-04 17:29:07 -0600

    fix: env config

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.ENV}` });
 
 function getEnvString(key: string): string {
   const value = process.env[key];
