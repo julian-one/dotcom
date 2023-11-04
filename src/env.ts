b0ce672eb6ace5bbb4dca8cb8891e2b4714938fe
Julian One <julian.roberts.one@gmail.com>
2023-11-04 13:09:02 -0600

    .

--- src/env.ts
+++ src/env.ts
@@ -25,7 +25,6 @@ function getEnvNumber(key: string): number {
 const env = {
   PORT: getEnvNumber('PORT'),
   SECRET: getEnvString('SECRET'),
-  ALLOWED_EMAILS: getEnvString('ALLOWED_EMAILS'),
   DB_USER: getEnvString('DB_USER'),
   DB_HOST: getEnvString('DB_HOST'),
   DB_NAME: getEnvString('DB_NAME'),
